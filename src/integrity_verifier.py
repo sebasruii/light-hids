490833b321b0a05e28b14e7fdd757468e75207ea
sebasruii <frabenrui1@alum.us.es>
2024-03-01 01:27:19 +0100

    yes it works

--- src/integrity_verifier.py
+++ src/integrity_verifier.py
@@ -6,10 +6,11 @@ def verificar_integridad(ruta, datos, backup_directory, indexed_folder):
     sha256_csv  = datos[2]
     sha1, sha256 = hash_generate.calcular_hashes(ruta)
     if sha1 != sha1_csv or sha256 != sha256_csv:
+        relative_path=ruta.split(indexed_folder)[1]
         print(f"El archivo {ruta} ha sido modificado.")
         file_name=ruta.split("/")[-1]
         logger.log_compromised_file(file_name,ruta)
-        restore.restore_file_from_backup(file_name, backup_directory, indexed_folder)
+        restore.restore_file_from_backup(relative_path, backup_directory, indexed_folder)
 
 
             
--- src/restore.py
+++ src/restore.py
@@ -2,12 +2,17 @@ import os
 import subprocess
 import shutil
 
-def restore_file_from_backup(file_name, backup_directory, indexed_folder):
+def restore_file_from_backup(relative_path, backup_directory, indexed_folder):
     # Construir la ruta completa del archivo en el directorio de respaldo
-    backup_file_path = os.path.join(backup_directory, file_name)
+    backup_file_path = os.path.join(backup_directory, relative_path[1:])
 
     # Construir la ruta de destino del archivo en la carpeta original
-    destination_file_path = os.path.join(indexed_folder, file_name)
+    destination_file_path = os.path.join(indexed_folder, relative_path[1:])
+    print(indexed_folder)
+    print(relative_path)
+    print(backup_directory)
+    print(destination_file_path)
+    print(backup_file_path)
 
     try:
         subprocess.run(['sudo', 'rm', destination_file_path])
