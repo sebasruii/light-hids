9e5feafffd79a926c452455d39fa8a1031ec8fbc
josgarber6 <josgarber6@alum.us.es>
2024-03-01 10:08:14 +0100

    Mejora del backup
    Ahora se hace bien el backup calcando el directorio de origen

--- src/backup.py
+++ src/backup.py
@@ -7,29 +7,29 @@ def backup_and_protect_files(indexed_folder, backup_directory):
         os.makedirs(backup_directory)
         subprocess.run(['sudo', 'chmod', 'u+w', backup_directory])  # Permitir escritura al usuario propietario
 
-        # Iterar sobre cada archivo dentro de la carpeta indexada
-        for root, dirs, files in os.walk(indexed_folder):
-            # Crear la estructura de directorios en el directorio de respaldo
-            relative_path = os.path.relpath(root, indexed_folder)
-            backup_directory_path = os.path.join(backup_directory, relative_path)
-            os.makedirs(backup_directory_path, exist_ok=True)
+    # Iterar sobre cada archivo dentro de la carpeta indexada
+    for root, dirs, files in os.walk(indexed_folder):
+        # Crear la estructura de directorios en el directorio de respaldo
+        relative_path = os.path.relpath(root, indexed_folder)
+        backup_directory_path = os.path.join(backup_directory, relative_path)
+        os.makedirs(backup_directory_path, exist_ok=True)
 
-            # Copiar archivos y establecer permisos
-            for file_name in files:
-                file_path = os.path.join(root, file_name)
-                # Construir la ruta de respaldo para el archivo
-                backup_file_path = os.path.join(backup_directory_path, file_name)
+        # Copiar archivos y establecer permisos
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            # Construir la ruta de respaldo para el archivo
+            backup_file_path = os.path.join(backup_directory_path, file_name)
 
-                try:
-                    # Copiar el archivo al directorio de respaldo
-                    subprocess.run(['sudo', 'cp', file_path, backup_file_path])
+            try:
+                # Copiar el archivo al directorio de respaldo
+                subprocess.run(['sudo', 'cp', file_path, backup_file_path])
 
-                    # Retirar los permisos de escritura y ejecución
-                    subprocess.run(['sudo', 'chmod', 'u-wx', backup_file_path])
-                    subprocess.run(['sudo', 'chmod', 'g-wx', backup_file_path])
-                    subprocess.run(['sudo', 'chmod', 'o-wx', backup_file_path])
-                except Exception as e:
-                    print(f"No se pudo hacer copia de seguridad de {file_path}: {e}")
+                # Retirar los permisos de escritura y ejecución
+                subprocess.run(['sudo', 'chmod', 'u-wx', backup_file_path])
+                subprocess.run(['sudo', 'chmod', 'g-wx', backup_file_path])
+                subprocess.run(['sudo', 'chmod', 'o-wx', backup_file_path])
+            except Exception as e:
+                print(f"No se pudo hacer copia de seguridad de {file_path}: {e}")
 
 def backup_file(file_path, backup_directory):
     # Extract file name from file path
@@ -55,3 +55,5 @@ def backup_file(file_path, backup_directory):
         print(f"Backup of {file_path} successful. Backup file location: {backup_file_path}")
     except Exception as e:
         print(f"Failed to backup {file_path}: {e}")
+
+backup_and_protect_files('/home/kali/Desktop/pruebaspai1/', '/home/kali/Desktop/PAI 1 files/backup/')
